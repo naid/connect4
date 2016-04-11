cd031f65406a8b20fa44f1ba08eacfe882351c36
ronald-framgia <ronald.garcia.magcalas@framgia.com>
2016-04-11 13:55:48 +0800

    loko

---
+++ public/js/game.code.js
@@ -0,0 +1,224 @@
+$( document ).ready( function() {
+    var player = 0,
+        brows = 6,
+        bcols = 7,
+        win = 4,
+        color = "red",
+        message = "it's " + color.toUpperCase() + "'s turn",
+        value = 0;
+        endgame = 0 ;
+
+// used to update player's turn
+    function updatePlayer( x ) {
+        player = x;
+        if (parseInt( x ) === 0){
+            color = "red";
+        }
+        else {
+            color = "blue";
+        }
+    }
+
+// adjust to available empty tile at bottom
+    function checkRowAvailability( tile ) {
+        var rowsel = tile.closest( "tr" ).attr( "brow" ),
+            colsel = tile.attr( "bcol" ),
+            tilesel = "tr.mrow-" + rowsel + " div.mcol-" + colsel,
+            lastrow = "";
+
+        for (var i = parseInt( rowsel ); i > 0; i--) {
+            tilesel = "tr.mrow-" + i + " div.mcol-" + colsel;
+            if ( $( tilesel ).hasClass( "clickable" )) {
+                lastrow = tilesel;
+            }
+        }
+
+        return lastrow;
+    }
+
+    function checkIfNextIsSameColor( ro, co, xdir, ydir, color, value ) {
+        var r = parseInt( ro ) + parseInt( ydir ),
+            c = parseInt( co ) + parseInt( xdir ),
+            tilesel = "tr.mrow-" + r + " div.mcol-" + c;
+
+        if ( $( ".clickable" ).hasClass( "clickable" ) ) {
+            if ($( tilesel ).hasClass("ui-state-highlight-" + color)) {
+    // value increases because same color
+                value = parseInt( value ) + 1;
+
+                if (value == win) {
+                    return parseInt( 1 );
+                }
+                return checkIfNextIsSameColor( r, c, xdir, ydir, color, value );
+            }
+            else
+            {
+                return parseInt( 0 );
+            }
+        }else{
+            return parseInt( 0 );
+        }
+    }
+
+// Board Template
+    function processTile( color, tiletarget ){
+
+        var prow = $( tiletarget ).closest( "tr" ).attr( "brow" ),
+            pcol = $( tiletarget ).attr( "bcol" );
+
+        if ((color == "blue") && (player == 1) ){
+            if($( tiletarget ).hasClass( "ui-state-highlight-red" )) {
+            }
+            else {
+                $( tiletarget ).addClass( "ui-state-highlight-blue" );
+                $( tiletarget ).addClass( "filled" );
+                $( tiletarget ).addClass( "temp-blue" );
+                $( tiletarget ).removeClass( "clickable" );
+                $( tiletarget ).removeClass( "ui-clickable" );
+                updatePlayer( 0 );
+            }
+            checkAll( prow, pcol, "blue" );
+        }
+
+        if((color == "red")&&(player === 0)){
+            if($( tiletarget ).hasClass( "ui-state-highlight-blue" ))
+            {
+            }
+            else {
+                $( tiletarget ).addClass( "ui-state-highlight-red" );
+                $( tiletarget ).addClass( "filled" );
+                $( tiletarget ).addClass( "temp-red" );
+                $( tiletarget ).removeClass( "clickable" );
+                $( tiletarget ).removeClass( "ui-clickable" );
+                updatePlayer( 1 );
+            }
+            checkAll( prow, pcol, "red" );
+        }
+    }
+
+// Check for Adjacent tiles
+    function newGame( )
+    {        
+        initialize();
+    }    
+
+// Check for Adjacent tiles
+    function checkAll( prow, pcol, color )
+    {
+        if(
+            checkIfNextIsSameColor( prow, pcol, 0, 1, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, 1, 1, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, 1, 0, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, 1, -1, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, 0, -1, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, -1, -1, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, -1, 0, color, 1 ) +
+            checkIfNextIsSameColor( prow, pcol, -1, 1, color, 1 ) >= 1
+        )
+        {
+            if ( endgame === 0 ){
+                $( "#result" ).text( color.toUpperCase() + " Wins!" );
+                $( ".clickable" ).addClass( "filled" );
+                $( ".clickable" ).removeClass( "clickable" );
+                endgame = 1;
+            }
+        }else{
+            if ( $( ".clickable" ).hasClass( "clickable" ) ){
+
+            }else{
+                if ( endgame === 0 ){
+                    $( "#result" ).text( " DRAW!" );
+                    $( ".clickable" ).addClass( "filled" );
+                    $( ".clickable" ).removeClass( "clickable" );
+                    endgame = 1;
+                }
+            }
+            
+        }
+    }
+
+        
+// Board Template
+    function createRows( x, y )
+    {
+        var tds = "",
+            trs = "",
+            table = "";
+
+        for (i = 1; i <= y; i++) {
+            tds += "<td>" +
+                "<div bcol = \"" +
+                i +
+                "\" class = \"" +
+                "d-tile clickable ui-widget-header mcol-" +
+                i +
+                "\">" +
+                    "&nbsp;" +
+//                    i +
+                "</div>" +
+            "</td>" + "";
+        }
+
+        for (i = x; i > 0; i--) {
+            trs +=
+            "<tr brow = \"" + i + "\" class = \"mrow-" + i + "\">" +
+//                "<th>" +
+//                i +
+//                "</th>" +
+                tds +
+            "</tr>" + "";
+        }
+
+        table = "<table>" +
+            trs +
+            "</table>";
+        return table;
+    }
+
+    function initialize(){
+// Board Template: Initialize each tile
+        endgame = 0;
+        $( "#board" ).html( createRows( brows, bcols ) );
+        $( "#message" ).text( message );
+        $( "#result" ).text( "" );
+
+        $( ".clickable" ).on( "click", function( ){
+
+            var tiletarget = checkRowAvailability($( this ));
+            processTile( color, tiletarget );
+            message = "it's " + color.toUpperCase() + "'s turn";
+            if( endgame < 1 ){
+                $( "#message" ).text( message );
+            }else
+            {
+                $( "#message" ).text( "" );
+            }
+            
+        });
+
+        $( ".clickable" ).on( "mouseover", function(){
+            $( this ).removeClass( "temp-red" );
+            $( this ).removeClass( "temp-blue" );
+
+            if($( this ).hasClass( "filled" )){
+            }
+            else {
+                $( this ).addClass("temp-" + color);
+            }
+        });
+
+        $( ".clickable" ).on( "mouseleave", function(){
+            $( this ).removeClass( "temp-red" );
+            $( this ).removeClass( "temp-blue" );
+        });
+
+        $( "#newgame" ).on( "click", function( ){
+            newGame( );
+        });
+        
+    }    
+
+
+initialize();
+
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,34 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
+        <title>Ronald Magcalas</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ URL::asset('css/style.css?4') }}" rel="stylesheet">
+        <script src="{{ URL::asset('js/game.code.js?3') }}"></script>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title"></div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Connect4 Game by Ronald Magcalas
+                        <button type="button" id='newgame' class="btn btn-success">
+                            New Game
+                        </button>
+                    </div>
+                    <div class="panel-body">
+                        <div id="board" class="ui-widget-content">
+                        </div>
+                        <br />
+                        <label id='message'></label>
+                        <label id='result'></label>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
